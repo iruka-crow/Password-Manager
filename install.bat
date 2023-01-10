d8d3e93481e7e34a90cff55432b7e310dba216cf
いるか <68815413+iruka-crow@users.noreply.github.com>
2023-01-11 00:23:42 +0900

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,9 +1,9 @@
 @echo off
 
 cd ..
-move .\Password-Manager %HOMEPATH%
+move .\Password-Manager-main %HOMEPATH%
 cd %HOMEPATH%
-cd .\Password-Manager\src\Windows
+cd .\Password-Manager-main\src\Windows
 move .\pass-manager C:\Windows\system32
 move .\passmg-uninstall C:\Windows\system32
 
