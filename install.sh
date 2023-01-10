213dabca7db6868ef1f6e9551d7046bd8984a7c8
いるか <68815413+iruka-crow@users.noreply.github.com>
2023-01-10 13:20:47 +0900

    Update install.sh
    Add an uninstall command

--- install.sh
+++ install.sh
@@ -4,7 +4,9 @@ git clone https://github.com/iruka-crow/Password-Manager
 sudo mv ./Password-Manager /etc/Password-Manager
 cd /etc/Password-Manager
 sudo mv ./pass-manager /usr/local/bin/pass-manager
+sudo mv ./pass-manager /usr/local/bin/passmg-uninstall
 
 sudo chmod 755 /usr/local/bin/pass-manager
+sudo chmod 755 /usr/local/bin/passmg-uninstall
 
 echo "Password-Manager Installed."
